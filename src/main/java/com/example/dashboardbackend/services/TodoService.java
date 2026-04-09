498036db4b7260f6154fc42dbfe7ed4cff2a627d
Kalousu <kevinbecker848@gmail.com>
2026-04-09 13:51:59 +0200

    feat: added functionality to add todos

---
+++ src/main/java/com/example/dashboardbackend/services/TodoService.java
@@ -0,0 +1,29 @@
+package com.example.dashboardbackend.services;
+
+import com.example.dashboardbackend.dtos.todo.TodoItemResponse;
+import com.example.dashboardbackend.models.widgets.todo.TodoItem;
+import com.example.dashboardbackend.repositories.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+    @Autowired
+    private TodoRepository todoRepository;
+
+    public List<TodoItemResponse> getTodosByWidgetId(Long widgetId){
+        List<TodoItem> items = todoRepository.findTodoItemsByWidgetItem_Id(widgetId)
+                .orElseThrow(() -> new RuntimeException("Todo-Items for id " + widgetId + " not found"));
+
+        List<TodoItemResponse> listResponse = items.stream().map(item -> new TodoItemResponse(
+                item.getId(),
+                item.getText(),
+                item.isCompleted(),
+                item.getSortOrder()
+        )).toList();
+
+        return listResponse;
+    }
+}
