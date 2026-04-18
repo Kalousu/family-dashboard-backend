1631b45ed35bbb9cf2a1ea9fc7b4976366380292
Kalousu <kevinbecker848@gmail.com>
2026-04-18 13:03:52 +0200

    feat: add exception handling for external apis

--- src/main/java/com/example/dashboardbackend/controllers/WeatherController.java
+++ src/main/java/com/example/dashboardbackend/controllers/WeatherController.java
@@ -7,6 +7,8 @@ import com.example.dashboardbackend.dtos.weather.WeatherRequest;
 import com.example.dashboardbackend.dtos.weather.WeatherResponse;
 import com.example.dashboardbackend.services.WeatherService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,13 +23,18 @@ public class WeatherController {
     private final WeatherService weatherService;
 
     @GetMapping
-    public WeatherResponse getWeather(@RequestParam double latitude, @RequestParam double longitude, @RequestParam String timezone) {
+    public ResponseEntity<WeatherResponse> getWeather(
+            @RequestParam double latitude,
+            @RequestParam double longitude, @RequestParam String timezone
+    ) {
         WeatherRequest weatherRequest = new WeatherRequest(latitude, longitude, timezone);
-        return weatherService.getWeather(weatherRequest);
+        return new ResponseEntity<>(weatherService.getWeather(weatherRequest), HttpStatus.OK);
     }
 
     @GetMapping("/search")
-    public List<GeoLocation> searchCities(@RequestParam String city) {
-        return weatherService.searchCities(city);
+    public ResponseEntity<List<GeoLocation>> searchCities(
+            @RequestParam String city
+    ) {
+        return new ResponseEntity<>(weatherService.searchCities(city), HttpStatus.OK);
     }
 }
