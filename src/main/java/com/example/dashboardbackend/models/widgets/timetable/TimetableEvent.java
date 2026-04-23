0ff97dd36ea8697778f632c2daebe18c3fb10f7e
Kalousu <kevinbecker848@gmail.com>
2026-04-23 11:56:09 +0200

    feat: bruh

--- src/main/java/com/example/dashboardbackend/models/widgets/timetable/TimetableEvent.java
+++ src/main/java/com/example/dashboardbackend/models/widgets/timetable/TimetableEvent.java
@@ -2,6 +2,8 @@ package com.example.dashboardbackend.models.widgets.timetable;
 
 import com.example.dashboardbackend.models.widgets.WidgetItem;
 import jakarta.persistence.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 import lombok.*;
 
 @Entity
@@ -17,6 +19,7 @@ public class TimetableEvent {
 
   @ManyToOne
   @JoinColumn(name = "widget_item_id")
+  @OnDelete(action = OnDeleteAction.CASCADE)
   private WidgetItem widgetItem;
 
   @Column(nullable = false)
